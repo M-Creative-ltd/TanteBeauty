7c2bf337ef225f21c5fecc692ecb6cb3f6bc1b7f
D-Murenzi <murenzidavid01@gmail.com>
2026-02-03 14:30:07 +0200

    update the icon favicon and the intro cta

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,8 +71,8 @@ export async function generateMetadata(): Promise<Metadata> {
       canonical: siteUrl,
     },
     icons: {
-      icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>',
-      apple: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>',
+      icon: ogImage ? ogImage : 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>',
+      apple: ogImage ? ogImage : 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>',
     },
   };
 }
